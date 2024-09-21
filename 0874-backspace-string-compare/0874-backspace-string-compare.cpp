d01c3463455daf44efecd4f28f2a987497ca47f2
Aditya Saw <117443804+heyitzmeaditya@users.noreply.github.com>
2024-09-21 18:53:31 +0530

    Time: 4 ms (24.76%), Space: 8.1 MB (40.18%) - LeetHub

---
+++ 0874-backspace-string-compare/0874-backspace-string-compare.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        stack<char> st1, st2;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] != '#') {
+                st1.push(s[i]);
+            } else {
+                if (!st1.empty()) {
+                    st1.pop();
+                }
+            }
+        }
+
+        for (int i = 0; i < t.size(); i++) {
+            if (t[i] != '#') {
+                st2.push(t[i]);
+            } else {
+                if (!st2.empty()) {
+                    st2.pop();
+                }
+            }
+        }
+
+        while (!st1.empty() && !st2.empty()) {
+            if (st1.top() != st2.top()) {
+                return false;
+            }
+            st1.pop();
+            st2.pop();
+        }
+
+        return st1.empty() && st2.empty();
+    }
+};

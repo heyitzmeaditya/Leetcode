582822f2854f7fa39812e1adae16a655702fd3c5
Aditya Saw <117443804+heyitzmeaditya@users.noreply.github.com>
2024-10-17 19:06:44 +0530

    Time: 9 ms (99.67%), Space: 13 MB (89.05%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        for (int i = 0; i < s.size(); i++) {
-            char c = s[i];
-            bool unique = true;
-            
-            
-            for (int j = 0; j < s.size(); j++) {
-                if (i != j && c == s[j]) {
-                    unique = false;
+        for(int i=0;i<s.size();i++){
+            char c=s[i];
+            bool unique= true;
+            for(int j=0;j<s.size();j++){
+                if(i!=j&&c==s[j]){
+                    unique=false;
                     break;
                 }
             }
-            
-            if (unique) {
-                return i;
+            if(unique){
+            return i;
             }
         }
+        return -1;
         
-        return -1; 
     }
 };

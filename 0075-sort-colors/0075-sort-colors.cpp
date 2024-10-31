a82036e0e7b376b9edc44b85f6cf41707d9d31ef
Aditya Saw <117443804+heyitzmeaditya@users.noreply.github.com>
2024-10-31 11:53:20 +0530

    Time: 0 ms (100%), Space: 10.3 MB (76.24%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,8 +1,14 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-         
+        for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[j]<nums[i]){
+                    swap(nums[i],nums[j]);
+                }
+            }
+        }
+        
         
     }
 };

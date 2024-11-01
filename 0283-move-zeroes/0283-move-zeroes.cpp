1151b487abbfd8bce89301ff235bfe60150dc134
Aditya Saw <117443804+heyitzmeaditya@users.noreply.github.com>
2024-11-01 19:48:39 +0530

    Time: 0 ms (100%), Space: 22 MB (26.38%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int i=0,j=0;
+        int start=0;
         for(int i=0;i<nums.size();i++){
-
             if(nums[i]!=0){
-                nums[j]=nums[i];
-                j++;
+                swap(nums[i],nums[start]);
+                start++;
             }
-        }
-        for(int i=j;i<nums.size();i++){
-            nums[i]=0;
         }
         
     }

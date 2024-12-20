daed73036cedeedd4fb72bf90462b1f4ca805d43
Aditya Saw <117443804+heyitzmeaditya@users.noreply.github.com>
2024-12-20 16:34:18 +0530

    Time: 0 ms (100%), Space: 9.2 MB (35.83%) - LeetHub

--- 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.cpp
+++ 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.cpp
@@ -2,16 +2,17 @@ class Solution {
 public:
     int maxChunksToSorted(vector<int>& arr) {
         int n=arr.size();
-        int chunks=0,prefixSum=0,sortedPrefixSum=0;
+        int chunks=0,sum=0,sortedsum=0;
         for(int i=0;i<n;i++){
-            prefixSum+=arr[i];
-            sortedPrefixSum+=i;
-            if(prefixSum==sortedPrefixSum){
+            sum+=arr[i];
+            sortedsum+=i;
+            if(sum==sortedsum){
                 chunks++;
+
+
             }
         }
         return chunks;
-
         
     }
 };

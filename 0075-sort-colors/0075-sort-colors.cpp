28dc347b17a49ca528f633ad49146bc780c87c94
Aditya Saw <117443804+heyitzmeaditya@users.noreply.github.com>
2025-03-11 14:43:35 +0530

    Time: 0 ms (100%), Space: 11.7 MB (36.64%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,14 +1,28 @@
 class Solution {
 public:
-//Insertion Sort
     void sortColors(vector<int>& nums) {
-        for(int i=1;i<nums.size();i++){
-            for(int j=i;j>0;j--){
-                if(nums[j]<nums[j-1]){
-                    swap(nums[j],nums[j-1]);
-                }
+        int count0=0,count1=0,count2=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0){
+                count0++;
+            }
+            else if(nums[i]==1){
+                count1++;
+            }
+            else{
+                count2++;
             }
         }
+        for(int i=0;i<count0;i++){
+            nums[i]=0;
+        }
+        for(int i=count0;i<count0+count1;i++){
+            nums[i]=1;
+        }
+        for(int i=count0+count1;i<count0+count1+count2;i++){
+            nums[i]=2;
+        }
+
         
     }
 };

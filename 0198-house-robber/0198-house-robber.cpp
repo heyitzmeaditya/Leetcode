5ed4bf37940841566be51b4d7594e0f9baffd2c7
Aditya Saw <117443804+heyitzmeaditya@users.noreply.github.com>
2025-05-31 19:05:49 +0530

    Time: 0 ms (100%), Space: 10.8 MB (5.88%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -7,10 +7,14 @@ public:
         if(n==0){
             return nums[0];
         }
-        if(dp[n]!=-1){
-            return dp[n];
+       
+        dp[0]=nums[0];
+        dp[1]=max(nums[0],nums[1]);
+        for(int i=2;i<=n;i++){
+            dp[i]=max(nums[i]+dp[i-2],dp[i-1]);
         }
-        return dp[n]=max(nums[n]+robbery(n-2,nums,dp),robbery(n-1,nums,dp));
+
+        return dp[n];
     }
     int rob(vector<int>& nums) {
         int n=nums.size();

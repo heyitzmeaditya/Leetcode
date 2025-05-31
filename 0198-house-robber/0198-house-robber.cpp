dc0692124136adf733fa6a38a219658622653f0b
Aditya Saw <117443804+heyitzmeaditya@users.noreply.github.com>
2025-05-31 18:24:28 +0530

    Time: 0 ms (100%), Space: 10.7 MB (22.1%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-int robber(int n,vector<int>&nums,vector<int>&dp){
-    if(n==0){
-        return nums[0];
-    }
-    if(n==1){
-        return max(nums[0],nums[1]);
-    }
-    if(dp[n]!=-1){
-        return dp[n];
-    }
-    return dp[n]=max(nums[n]+robber(n-2,nums,dp),robber(n-1,nums,dp));
+    int robbery(int n,vector<int>&nums,vector<int>&dp){
+        if(n<0){
+            return 0;
+        }
+        if(n==0){
+            return nums[0];
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        return dp[n]=max(nums[n]+robbery(n-2,nums,dp),robbery(n-1,nums,dp));
     }
     int rob(vector<int>& nums) {
         int n=nums.size();
         vector<int>dp(n,-1);
-        return robber(n-1,nums,dp);
+        return robbery(n-1,nums,dp);
+        
     }
 };
